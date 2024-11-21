e2367a4740f2b88b651a6c5d0acb33cead41c720
jaybat-code <jacobbattenfielddev@gmail.com>
2024-11-21 09:59:00 -0600

    first hour in.

---
+++ React Basics/React-Basics/src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client"
+import './index.css'
+root = createRoot(document.getElementById("root"))
+const text = <h1>This is so fucking sick!</h1>
+
+root.render(
+  text
+)
